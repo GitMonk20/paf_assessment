97c191b6bc9f3cdae23ae79bcc6a3b69e99eeeda
GitMonk20 <ravishankarr2022@outlook.com>
2023-04-28 16:11:04 +0800

    Updated Databases

--- database/create.sql
+++ database/create.sql
@@ -1,3 +1,19 @@
 -- Task 1
 -- Write SQL statements in this file to create the brewery database and 
 -- populate the database with the given SQL files
+
+-- Create the brewery database
+CREATE DATABASE brewery;
+
+-- Use the brewery database
+USE brewery;
+
+-- Execute the provided SQL files
+SOURCE beers.sql;
+SOURCE breweries.sql;
+SOURCE categories.sql;
+SOURCE geocodes.sql;
+SOURCE styles.sql;
+
+-- Connect with "mysql -h hostname -u username -p"
+-- Run with "source create.sql"
